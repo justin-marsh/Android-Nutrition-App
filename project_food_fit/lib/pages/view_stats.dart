8d75e9ae76b6d374d0dd2c848344afd92f860eeb
justin-marsh <justin.marsh@ontariotechu.net>
2023-12-06 23:19:36 -0500

    nav bar fix

--- project_food_fit/lib/pages/view_stats.dart
+++ project_food_fit/lib/pages/view_stats.dart
@@ -3,6 +3,8 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:project_food_fit/pages/profile.dart';
+import 'package:project_food_fit/pages/searchpage.dart';
+import 'package:project_food_fit/pages/home.dart';
 
 void main() {
   runApp(ViewStatsPage());
@@ -106,6 +108,52 @@ class _PreferencesScreenState extends State<PreferencesScreen> {
             );
           }
         },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: IconButton(
+              icon: Icon(Icons.home, color: Colors.grey),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+            ),
+            label: '', // Empty label
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              icon: Icon(Icons.search, color: Colors.grey),
+              onPressed: () {
+                // Navigate to the profile page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SearchPage()),
+                );
+              },
+            ),
+
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: CustomPlusIcon(),
+            label: '', // Empty label
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite, color: Colors.grey),
+            label: '', // Empty label
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Color(0xFFFF785B)),
+            label: '', // Empty label
+          ),
+        ],
       ),
     );
   }
@@ -265,6 +313,25 @@ class _PreferencesScreenState extends State<PreferencesScreen> {
           fontWeight: FontWeight.bold,
         ),
         textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
+class CustomPlusIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 48,
+      height: 48,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Color(0xFFFF785B),
+      ),
+      child: Center(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
     );
   }
